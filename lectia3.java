be68ffc4198e83cb53afaafb7eaadb00f45e7df7
Viorelmd <viorel-1998@mail.ru>
2017-09-19 13:08:27 +0300

    Update lectia3.java

--- lectia3.java
+++ lectia3.java
@@ -1,7 +1,7 @@
 package com.company;
 import java.util.Scanner;
 
-public class Main {
+public class Lectia3 {
     public static void main(String[] args) {
     Scanner key = new Scanner(System.in);
 	System.out.print("Scrie nr de la sarcina 1..11: ");
